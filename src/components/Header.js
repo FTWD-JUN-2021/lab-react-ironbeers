24e7325ba0e0c3c1fd06fb9746bc61623e683bdb
avanalphen <vanalphena@gmail.com>
2021-07-01 16:42:08 -0400

    it 2

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header(props) {
+    return (
+        <div>
+            <nav style = {{background: `#1DA1F2`, padding: `2em`}}>
+                🏠
+            </nav>
+        </div>
+    );
+}
+
+export default Header;
